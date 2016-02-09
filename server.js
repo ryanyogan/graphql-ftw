ff1f9bf0b09809c76effa2147d601505583ff23b
Ryan Yogan <ryanyogan@gmail.com>
2016-02-08 18:40:55 -0600

    minor cleanup, ditched CB in favor of async from stage-0

--- server.js
+++ server.js
@@ -9,23 +9,13 @@ import { MongoClient } from 'mongodb';
 const app = express();
 app.use(express.static('public'));
 
-
-let db;
-MongoClient.connect(process.env.MONGO_URL, (err, database) => {
-  if (err) throw err;
-  db = database;
+(async () => {
+  const db = await MongoClient.connect(process.env.MONGO_URL);
 
   app.use('/graphql', GraphQLHTTP({
     schema: schema(db),
     graphiql: true
   }));
+
   app.listen(3000, () => console.log('Listening on port 3000'));
-});
-
-app.get('/data/links', (req, res) => {
-  db.collection('links').find({}).toArray((err, links) => {
-    if (err) throw err;
-
-    res.json(links);
-  });
-});
+})();
